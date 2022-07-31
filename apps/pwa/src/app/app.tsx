4e89c1cb91664186eec6ca2a0e759d995ef6ac6d
yurii.badger <badger.lviv.ua@gmail.com>
2022-07-31 09:18:09 +0300

    Changed welcome page title

--- apps/pwa/src/app/app.tsx
+++ apps/pwa/src/app/app.tsx
@@ -1,11 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="pwa" />
+      <NxWelcome title="to Nx React PWA" />
       <div />
     </>
   );
